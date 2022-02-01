ab11ee39e323ca06c4cb1a0ce00c7af22699c438
JinBay2929 <jinbay2929@gmail.com>
2022-02-01 14:08:32 +0900

    2022/02/01

---
+++ AtCoder/ABS/practiceA.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<stdio.h>
+
+
+using namespace std;
+
+
+int main(){
+    int a,b,c;
+    string s;
+
+    cin>>a>>b>>c>>s;
+
+    cout<<a+b+c<<" "<<s<<endl;
+    
+    return 0;
+}
