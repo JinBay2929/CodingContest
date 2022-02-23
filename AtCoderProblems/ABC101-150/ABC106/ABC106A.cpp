b75e593abfc7f82f6c0313788d3c68952f4bdcb2
JinBay2929 <jinbay2929@gmail.com>
2022-02-23 20:45:02 +0900

    2022/02/23

---
+++ AtCoderProblems/ABC101-150/ABC106/ABC106A.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+#include<cmath>
+#include<map>
+
+using namespace std;   
+
+
+
+int main(){
+    int a, b;
+    int ans;
+    cin>>a>>b;
+
+    ans=(a-1) * (b-1);
+    
+    cout<<ans<<endl;
+    return 0;
+}
