851ddbb112f7b87723f5403e6e0b8c8de4205a8b
JinBay2929 <jinbay2929@gmail.com>
2022-02-24 23:30:20 +0900

    2022/02/24

---
+++ AtCoderProblems/ABC101-150/ABC107/ABC107A.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+#include<cmath>
+#include<map>
+
+using namespace std;   
+
+
+
+int main(){
+    int n, i, ans;
+    
+    cin>>n>>i;
+
+    ans=n-i+1;
+    
+    cout<<ans<<endl;
+    return 0;
+}
