d0a04f45070766d68927b9cbfc8ef49ee2c1dc1d
JinBay2929 <jinbay2929@gmail.com>
2022-11-08 23:30:50 +0900

    modified

--- AOJ/prac.cpp
+++ AOJ/prac.cpp
@@ -15,69 +15,15 @@
 using namespace std;   
 
 
-// 要素が複数の場合
-struct Place {
-  int x;
-  int y;
-  char lr;
-};
-
-struct PlaceLess { // 大小比較用の関数オブジェクトを定義することもできる
-  bool operator()(const Place& a, const Place& b) const noexcept {
-    // キーとして比較したい要素を列挙する
-    return std::tie(a.y) < std::tie(b.y);
-  }
-};
 int main(){
-  int n, x, y;
-  string s, ans="No";
-  vector<vector<int> > places;
+  int n, a, b;
   cin>>n;
-
-  for(int i=0; i<n; i++){
-    vector<int> tmp;
-    cin>>x>>y;
-    tmp.push_back(x);
-    tmp.push_back(y);
-    places.push_back(tmp);
+  if(n==1){
+    cout<<"Hello World"<<endl;
+  }else{
+    cin>>a>>b;
+    cout<<a+b<<endl;
   }
-  cin>>s;
-
-  vector<Place> vp;
-  for(int i=0; i<n; i++){
-    vp.push_back(Place{places[i][0], places[i][1], s[i]});
-  }
-
-  sort(vp.begin(), vp.end(), PlaceLess{});
-
-  // for(int i=0; i<vp.size(); i++){
-  //   cout<<vp[i].x<<vp[i].y<<vp[i].lr<<endl;
-  // }
-
-  int tmpy=-1, starti=-21;
-  for(int i=0; i<n; i++){
-    
-    if(tmpy==vp[i].y){
-      int ts=i-starti;
-      for(int j=0; j<ts; j++){
-        if(vp[j+starti].x<vp[i].x && vp[j+starti].lr=='R' && vp[i].lr=='L'){
-          ans="Yes";
-          break;
-        }
-        if(vp[j+starti].x>vp[i].x && vp[j+starti].lr=='L' && vp[i].lr=='R'){
-          ans="Yes";
-          break;
-        }
-      }
-    }else{
-      starti=i;
-      tmpy=vp[i].y;
-    }
-  }
-  
-
-  
-  cout<<ans<<endl;
   
   return 0;
 }
